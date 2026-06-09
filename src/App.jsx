ed90bd5c45bbf1ad0e8babd4107f2fd64cecc20c
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-06-09 17:35:52 +0200

    Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,49 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [products, setProducts] = useState([]);
+
+  const callProducts = async (query) => {
+    const res = await fetch(`http://localhost:3333/products?search=${query}`);
+    const data = await res.json();
+    console.log(data);
+    return setProducts(data);
+  };
+
+  useEffect(() => {
+    if (inputText !== '') {
+      callProducts(inputText);
+    } else {
+      setProducts([]);
+    }
+  }, [inputText]);
+
   return (
     <div className="container mt-5">
-      <h1>Ciao</h1>
+      <h1>Ecco i prodotti</h1>
+
+      <input
+        type="text"
+        onChange={(e) => setInputText(e.target.value)}
+        value={inputText}
+        placeholder="Cerca il prodotto"
+      />
+
+      <div className="row">
+        <h2>Suggestions</h2>
+        {products.map((p, i) => {
+          return (
+            <div className="col" key={i}>
+              <p>{p.name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
